343578ecd7c61d059c2fba5ef70542071a840108
Zamin ali <162513204+Zaminali1@users.noreply.github.com>
2025-06-04 01:50:05 +0500

    Update file i'o.py

--- Day 1/file i'o.py
+++ Day 1/file i'o.py
@@ -6,30 +6,30 @@ with open("hey.txt","r") as f:
 
 
 
-# import json
-# emplist={
-#   "name": "Zamin",
-#   "age": 17,
-#   "skills": ["Python", "AI"]
-# }
+import json
+emplist={
+  "name": "Zamin",
+  "age": 17,
+  "skills": ["Python", "AI"]
+}
 
-# try:
-#      with open("hy.json","w") as fh:
-#       json.dump(emplist,fh)
-#       print(fh)
-# except FileExistsError:
-# #     print("That file already exists")
+try:
+     with open("hy.json","w") as fh:
+      json.dump(emplist,fh)
+      print(fh)
+except FileExistsError:
+#     print("That file already exists")
 
 
-# import json
-# emplist={
-#   "name": "Zamin",
-#   "age": 17,
-#   "skills": ["Python", "AI"]
-# }
-# with open("hhj.json","w") as fl:
-#     json.dump(emplist,fl,indent=6)
-#     print(fl)
+import json
+emplist={
+  "name": "Zamin",
+  "age": 17,
+  "skills": ["Python", "AI"]
+}
+with open("hhj.json","w") as fl:
+    json.dump(emplist,fl,indent=6)
+    print(fl)
 
 
 
